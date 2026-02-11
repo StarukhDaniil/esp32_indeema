109e3c13d1310c2839840cdef9d2fc2b7b791750
StarukhDaniil <starukh.danil@gmail.com>
2026-02-11 12:44:34 +0200

    Created 2 task for sending and receiving data

--- main/main.c
+++ main/main.c
@@ -1,34 +1,52 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include <esp_random.h>
 
-void vTaskCode(void *pvParameters) {
-    printf("Custom task started\n");
-    uint32_t counter = 0;
+typedef struct {
+    uint32_t deviceID;
+    uint32_t measurementID;
+    float temperature;
+} DataPackage_t;
+
+QueueHandle_t xSensorQueue;
+
+void task_sender(void* pvParameters) {
+    DataPackage_t dataToSend;
+    dataToSend.deviceID = 101;
+    dataToSend.measurementID = 0;
+
     for ( ;; ) {
-        printf("Counter: %lu\n", counter);
-        ++counter;
-        vTaskDelay(pdMS_TO_TICKS(100));
+        dataToSend.temperature = 20.0 + (float)(esp_random() % 100) / 10.0;
+        ++dataToSend.measurementID;
+        printf("[Sender] Sending measurement #%lu (Temp: %.2f)\n",
+            dataToSend.measurementID, dataToSend.temperature);
+        
+        if (xQueueSend(xSensorQueue, &dataToSend, portMAX_DELAY) != pdPASS) {
+            printf("[SENDER] Failed to send");
+        }
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
+
+void task_receiver(void* pvParameters) {
+    DataPackage_t receivedData;
+    for ( ;; ) {
+        if (xQueueReceive(xSensorQueue, &receivedData, portMAX_DELAY) == pdTRUE) {
+            printf("[Receiver] Received data! Device: %lu | ID: %lu | Temp: %.2f\n",
+                receivedData.deviceID, receivedData.measurementID, receivedData.temperature);
+        }
     }
 }
 
 void app_main(void)
 {
-    TaskHandle_t xHandle = NULL;
-
-    xTaskCreate(
-        vTaskCode,
-        "IDLE_TASK",
-        2048,
-        NULL,
-        tskIDLE_PRIORITY,
-        &xHandle
-    );
-
-    uint32_t counter = 0;
-    for ( ;; ) {
-        printf("Counter: %lu\n", counter);
-        ++counter;
-        vTaskDelay(pdMS_TO_TICKS(500));
+    xSensorQueue = xQueueCreate(5, sizeof(DataPackage_t));
+    if (xSensorQueue == NULL) {
+        printf("Error while creating a queue!\n");
+        return;
     }
+
+    xTaskCreatePinnedToCore(task_sender, "SENDER", 2048, NULL, 1, NULL, 0);
+    xTaskCreatePinnedToCore(task_receiver, "RECEIVER", 2048, NULL, 1, NULL, 1);
 }
