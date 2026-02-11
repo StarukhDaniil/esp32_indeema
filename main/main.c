3f9f1163f4d051240f7045786233b317585e6d1e
StarukhDaniil <starukh.danil@gmail.com>
2026-02-11 12:21:15 +0200

    Added task to run parallel

--- main/main.c
+++ main/main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void vTaskCode(void *pvParameters) {
+    printf("Custom task started\n");
+    uint32_t counter = 0;
+    for ( ;; ) {
+        printf("Counter: %lu\n", counter);
+        ++counter;
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
 
 void app_main(void)
 {
-    printf("asdasdasd");
+    TaskHandle_t xHandle = NULL;
+
+    xTaskCreate(
+        vTaskCode,
+        "IDLE_TASK",
+        2048,
+        NULL,
+        tskIDLE_PRIORITY,
+        &xHandle
+    );
+
+    uint32_t counter = 0;
+    for ( ;; ) {
+        printf("Counter: %lu\n", counter);
+        ++counter;
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
 }
