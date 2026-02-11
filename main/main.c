ef2a6bcfc1cfd3ace775066cbf4b8dce9ab7e498
StarukhDaniil <starukh.danil@gmail.com>
2026-02-11 13:28:42 +0200

    Added 2 tasks to load CPU

--- main/main.c
+++ main/main.c
@@ -1,52 +1,70 @@
 #include <stdio.h>
+#include <esp_random.h>
+#include <esp_rom_sys.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include <esp_random.h>
 
 typedef struct {
-    uint32_t deviceID;
-    uint32_t measurementID;
-    float temperature;
-} DataPackage_t;
+    uint32_t workerID;
+    uint32_t workTimeMS;      // how much time to load CPU in milli-seconds
+    uint32_t restTimeMS;      // how much time not to load CPU in milli-seconds  
+} workerInfo_t;
 
-QueueHandle_t xSensorQueue;
-
-void task_sender(void* pvParameters) {
-    DataPackage_t dataToSend;
-    dataToSend.deviceID = 101;
-    dataToSend.measurementID = 0;
+void worker(void* pvParameters) {
+    const workerInfo_t* info = pvParameters;
 
     for ( ;; ) {
-        dataToSend.temperature = 20.0 + (float)(esp_random() % 100) / 10.0;
-        ++dataToSend.measurementID;
-        printf("[Sender] Sending measurement #%lu (Temp: %.2f)\n",
-            dataToSend.measurementID, dataToSend.temperature);
+        printf("[Worker #%lu] Work started...\n", info->workerID);
         
-        if (xQueueSend(xSensorQueue, &dataToSend, portMAX_DELAY) != pdPASS) {
-            printf("[SENDER] Failed to send");
-        }
-        vTaskDelay(pdMS_TO_TICKS(2000));
-    }
-}
+        esp_rom_delay_us(info->workTimeMS * 1000);      // multiplying by 1000 since we have to convert from ms to us
 
-void task_receiver(void* pvParameters) {
-    DataPackage_t receivedData;
-    for ( ;; ) {
-        if (xQueueReceive(xSensorQueue, &receivedData, portMAX_DELAY) == pdTRUE) {
-            printf("[Receiver] Received data! Device: %lu | ID: %lu | Temp: %.2f\n",
-                receivedData.deviceID, receivedData.measurementID, receivedData.temperature);
-        }
+        printf("[Worker #%lu] Work ended...\n", info->workerID);
+
+        vTaskDelay(pdMS_TO_TICKS(info->restTimeMS));
     }
 }
 
 void app_main(void)
 {
-    xSensorQueue = xQueueCreate(5, sizeof(DataPackage_t));
-    if (xSensorQueue == NULL) {
-        printf("Error while creating a queue!\n");
-        return;
-    }
+    TaskHandle_t worker1 = NULL;
+    TaskHandle_t worker2 = NULL;
 
-    xTaskCreatePinnedToCore(task_sender, "SENDER", 2048, NULL, 1, NULL, 0);
-    xTaskCreatePinnedToCore(task_receiver, "RECEIVER", 2048, NULL, 1, NULL, 1);
+    workerInfo_t workerInfo1 = {
+        .restTimeMS = 200,
+        .workTimeMS = 100,
+        .workerID = 1
+    };
+
+    workerInfo_t workerInfo2 = {
+        .restTimeMS = 100,
+        .workTimeMS = 400,
+        .workerID = 2
+    };
+
+    xTaskCreate(
+        worker,
+        "WORKER1",
+        2048,
+        &workerInfo1,
+        1,
+        &worker1);
+    
+    xTaskCreate(
+        worker,
+        "WORKER2",
+        2048,
+        &workerInfo2,
+        1,
+        &worker2);
+    
+    char stats_buffer[1024];
+
+    for( ;; ) {
+        vTaskGetRunTimeStats(stats_buffer);
+        printf("\nTask Name\tAbs Time\tTime %%\n");
+        printf("------------------------------------------\n");
+        printf("%s", stats_buffer);
+
+        vTaskDelay(pdMS_TO_TICKS(5000));
+    }
 }
